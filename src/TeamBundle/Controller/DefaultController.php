78d44f242d3540d8014abe3faeef4ba9672ce0e8
Amira Doghri <doghriamira@gmail.com>
2020-05-05 00:48:13 +0100

    uniqueentity on team_user

--- src/TeamBundle/Controller/DefaultController.php
+++ src/TeamBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace TeamBundle\Controller;
 
 use MainBundle\Entity\User;
+use Swift_Message;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use TeamBundle\Entity\team_user;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -10,8 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use TeamBundle\Entity\team;
+
 
 
 class DefaultController extends Controller
@@ -33,7 +33,7 @@ class DefaultController extends Controller
                     return $this->render('@Team/team/existedeja.html.twig', array("id" => $id, "users" => $users));
 
                 } else {
-                    $message = \Swift_Message::newInstance()
+                    $message = Swift_Message::newInstance()
                         ->setSubject('Affectation au team sur la plateforme Scrumify')
                         ->setFrom('iheb.rekik@esprit.tn')
                         ->setTo($user->getEmail())
@@ -56,45 +56,18 @@ class DefaultController extends Controller
         $users= $this->getDoctrine()->getRepository('MainBundle:User')->findAll();
         $user= $this->getDoctrine()->getRepository('MainBundle:User')->find($user_id);
         $team= $this->getDoctrine()->getRepository('TeamBundle:team')->find($team_id);
-        $con3 = $this->getDoctrine()->getRepository('TeamBundle:team_user')->findBy(array('teamId' => $team_id));
 
-       if ( $con3 != null) {
-           foreach ($con3 as $con) {
-               if ($con->getUserId() != $user_id) {
-                   if ($con->getUserId()->getEmail() == $user->getEmail()) {
-                       return $this->render('@Team/team/ajout1.html.twig', array("id" => $team_id, "users" => $users));
-                   } else {
-                       //envoyer mail notif
-                       $message = \Swift_Message::newInstance()
-                           ->setSubject('Affectation au team sur la plateforme Scrumify')
-                           ->setFrom('iheb.rekik@esprit.tn')
-                           ->setTo($user->getEmail())
-                           ->setBody(
-                               $this->renderView('@MyAppMail/Mail/mail.html.twig',
-                                   array('team' => $team->getName(), 'text/html')));
-                       $this->get('mailer')->send($message);
+        $con3 = $this->getDoctrine()->getRepository('TeamBundle:team_user')->findBy(array('teamId' => $team_id,'userId'=>$user_id));
 
-                       // affectation le user dans l'equipe
-                       $aff = new team_user();
-                       $aff->setTeamId($team);
-                       $aff->setUserId($user);
-                       $aff->setRole(1);
-                       $em = $this->getDoctrine()->getManager();
-                       $em->persist($aff);
-                       $em->flush();
-                   }
+        if ( $con3 != null) {
 
+            return $this->render('@Team/team/ajout1.html.twig', array("id" => $team_id, "users" => $users));
 
-               } else {
-                   return $this->render('@Team/team/ajout1.html.twig', array("id" => $team_id, "users" => $users));
-
-               }
-
-           }
-       }
-       else {
+        }
+       else
+       {
            //envoyer mail notif
-           $message = \Swift_Message::newInstance()
+           $message = Swift_Message::newInstance()
                ->setSubject('Affectation au team sur la plateforme Scrumify')
                ->setFrom('iheb.rekik@esprit.tn')
                ->setTo($user->getEmail())
@@ -111,12 +84,19 @@ class DefaultController extends Controller
            $em = $this->getDoctrine()->getManager();
            $em->persist($aff);
            $em->flush();
+           return $this->render('@Team/team/ajoutTeam.html.twig',array("id" => $team_id,"users"=>$users));
+
        }
 
 
 
 
-        return $this->render('@Team/team/ajoutTeam.html.twig',array("id" => $team_id,"users"=>$users));
+
+
+
+
+
+
 
     }
 
